82b66242ce94751a79931ce3ee664f59f84124fb
Harby Garcia <harby-96@hotmail.com>
2025-08-24 23:54:09 -0500

    configuracion application.yaml y mapeo correcto en UserEntity

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/entity/UserEntity.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/entity/UserEntity.java
@@ -1,7 +1,8 @@
 package co.com.crediya.r2dbc.entity;
 
-import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDate;
@@ -18,9 +19,12 @@ public class UserEntity {
     private Long id;
     private String nombres;
     private String apellidos;
+    @Column("fecha_nacimiento")
     private LocalDate fechaNacimiento;
     private String direccion;
     private String telefono;
+    @Column("correo_electronico")
     private String correoElectronico;
+    @Column("salario_base")
     private Double salarioBase;
 }
